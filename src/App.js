69061ecdceef5c0c6f6dd8944f4813ff91d854e5
Victor Malinov <victor@mlnova.com>
2022-03-21 12:11:39 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Routes>
+      <Route exact path="/" element="This is home page!" />
+      <Route exact path="/starred" element="This is starred!" />
+      <Route path="*" element="This is 404 page" />
+    </Routes>
+  );
 }
 
 export default App;
