4d765151ecfb59d6dba585a91aef2dd71442582b
Victor Malinov <victor@mlnova.com>
2022-03-21 18:02:22 +0200

    added layout managment and created title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element="This is home page!" />
-      <Route exact path="/starred" element="This is starred!" />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/starred" element={<Starred />} />
       <Route path="*" element="This is 404 page" />
     </Routes>
   );
