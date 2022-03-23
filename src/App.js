a26160e92d3e6a761b64421fdbe01b5875f88c37
Victor Malinov <victor@mlnova.com>
2022-03-23 18:05:39 +0200

    added show page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Show from './pages/Show';
 import Starred from './pages/Starred';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/starred" element={<Starred />} />
+      <Route exact path="/show/:id" element={<Show />} />
       <Route path="*" element="This is 404 page" />
     </Routes>
   );
